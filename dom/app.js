2dc0ca8061724ede297dd06505696df343d77e5d
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-03-07 17:34:04 +0200

    update

--- dom/app.js
+++ dom/app.js
@@ -1,3 +1,40 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
+// console.log (document.title);
+// console.log (document.doctype);
+// console.log (document.forms);
+// console.log (document.links);
+
+// document.getElementById
+// let input = document.getElementById ('exampleInputEmail');
+// input.value= 'Bu bir input';
+// input.className ="test-class";
+
+// console.log (input);
+
+
+
+// document.getElementsByClassName
+
+
+// document.getElementsByTagName
+
+
+// document.getElementsByName
+
+// let item= document.querySelector('.list-group-item');
+// console.log(item.parentElement);
+
+// //CREATE ELEMENT
+// let button =document.createElement('button');
+// button.className ='btn btn-success';
+// button.interHTML ="Created button";
+// button.setAttribute ('id', "yeni button");
+
+// let div = document.querySelector ('#test');
+
+// div.appendChild(button);
+
+
+
+
+
+
